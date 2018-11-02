cc400d4afc8c24249fa6ab2b1970f85dd306737e
ninofelino <tutyfelino@gmail.com>
2018-11-02 11:29:30 +0700

    jumat

--- models/models.py
+++ models/models.py
@@ -2,6 +2,16 @@
 
 from odoo import models, fields, api
 
+class myconf(models.Model):
+    _name='myconferences.myconf'
+    _description='a model for a conference'
+    namabarang=fields.Char('Article' , Required=True)
+    category=fields.Char('Category' , Required=True)
+    indexed=fields.Boolean('Variant', Required=True)
+    startdate=fields.Date('Start Date', Required=True)
+    enddate=fields.Date('End Date', Required=True)    
+    fee=fields.Float('Registration Fee', Required=True)
+
 # class felino(models.Model):
 #     _name = 'felino.felino'
 
